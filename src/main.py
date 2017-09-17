885785cd2539ed4b67ceacf58c23a40d8d5e348e
Simon Shapiro <simon.m.shapiro@gmail.com>
2017-09-17 18:15:38 +0100

    Csv read and write to re-duce memory used

--- src/main.py
+++ src/main.py
@@ -3,13 +3,19 @@ from lib.DQpy import test_row
 import csv
 
 input_filenm = "/Users/simonshapiro/DQpy/data/enhanced.ledger.sdi.log"
-output_filenm = ""
-results = []
-with open(input_filenm, "r") as input_file:
-    reader = csv.DictReader(input_file, delimiter='|')
-    for row in reader:
-#        print(row)
-        result = test_row(row, ledger_sdi_ruleset.rule_set, verbose=False)
-#        print(result)
-        if result: results.append(result)
-print(results)
+output_filenm = "/Users/simonshapiro/DQpy/data/enhanced.ledger.dq.csv"
+with open(output_filenm, "w") as output_file:
+    with open(input_filenm, "r") as input_file:
+        reader = csv.DictReader(input_file, delimiter='|')
+        first_read = True
+        for row in reader:
+    #        print(row)
+            result = test_row(row, ledger_sdi_ruleset.rule_set, verbose=False)
+    #        print(result)
+            if result:
+                if first_read:
+                    header = result[0].keys()
+                    writer = csv.DictWriter(output_file, delimiter=',', fieldnames=header)
+                    writer.writeheader()
+                    first_read = False
+                writer.writerows(result)
